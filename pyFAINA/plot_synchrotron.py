c7074e96b5217801d7efc7c1758f60fc09ecea24
romanskyvadim <romanskyvadim@gmail.com>
2024-06-13 23:43:12 +0300

    pyfaina

--- pyFAINA/plot_synchrotron.py
+++ pyFAINA/plot_synchrotron.py
@@ -14,9 +14,20 @@ def plot_synchrotron():
         radiation[0,i] = float(s[0])
         radiation[1,i] = float(s[1])
 
-    cssx1 = [1.5, 3.0, 6.1, 9.87]
-    cssy1 = [1.5, 4.3, 6.1, 4.2]
-    cssError1 = [0.1, 0.2, 0.3, 0.2]
+    cssFile = open('../examples_data/css_data/coppejans99.txt', 'r')
+    lines = cssFile.readlines()
+    Ncss = len(lines)
+
+    css = np.zeros([3, Ncss])
+    for i in range(Ncss):
+        s = lines[i].split()
+        css[0, i] = float(s[4])
+        css[1, i] = float(s[6])
+        css[2, i] = float(s[7])
+
+    #cssx1 = [1.5, 3.0, 6.1, 9.87]
+    #cssy1 = [1.5, 4.3, 6.1, 4.2]
+    #cssError1 = [0.1, 0.2, 0.3, 0.2]
 
 
 
@@ -38,6 +49,6 @@ def plot_synchrotron():
     ax.minorticks_on()
     # plt.axis([0.0,1.0,0.0,1.0])
     plt.plot(radiation[0], radiation[1], 'r', linewidth=4)
-    plt.errorbar(cssx1, cssy1, cssError1, ecolor = 'b', elinewidth = 4, linewidth=0, capsize = 5, capthick = 4)
+    plt.errorbar(css[0,:], css[1,:], css[2,:], ecolor = 'b', elinewidth = 4, linewidth=0, capsize = 5, capthick = 4)
     #plt.show()
     plt.savefig('synchrotron1.png', bbox_inches='tight')
