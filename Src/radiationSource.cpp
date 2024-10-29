f4dabc67c2c500b9d4483feac1beeb05dda31ed6
vadimromansky <romanskyvadim@gmail.com>
2024-10-29 19:24:30 +0300

    fz

--- Src/radiationSource.cpp
+++ Src/radiationSource.cpp
@@ -2259,7 +2259,12 @@ void TabulatedSLSourceWithSynchCutoff::updateLB2() {
 				double rho = (irho + 0.5) * drho;
 				double z = (iz + 0.5) * dz - my_rho;
 				double theta = acos(z / sqrt(rho * rho + z * z));
-				my_LB2[irho][iz][iphi] = evaluateTotalLB2fromPoint(rho, z, iphi, theta);
+				//my_LB2[irho][iz][iphi] = evaluateTotalLB2fromPoint(rho, z, iphi, theta);
+				double l = my_rho - rho;
+				if (l < 0) {
+					l = 0;
+				}
+				my_LB2[irho][iz][iphi] = my_B[irho][iz][iphi] * my_B[irho][iz][iphi] * l;
 			}
 		}
 	}
